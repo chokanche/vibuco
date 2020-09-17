4475879fa66cae75cd7e478c059e30b38127c97c
Matthijs Kralt <matthijs29@live.com>
2020-09-17 21:43:39 +0200

    Add PUBLIC_BUCKET_NAME to env

--- config.js
+++ config.js
@@ -1,12 +1,13 @@
-import getConfig from 'next/config';
+import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
 
 export const USER_POOL_REGION = publicRuntimeConfig.USER_POOL_REGION;
 export const USER_POOL_ID = publicRuntimeConfig.USER_POOL_ID;
 export const USER_POOL_CLIENT_ID = publicRuntimeConfig.USER_POOL_CLIENT_ID;
+export const PUBLIC_BUCKET_NAME = publicRuntimeConfig.PUBLIC_BUCKET_NAME;
+export const IDENTITY_POOL_ID = publicRuntimeConfig.IDENTITY_POOL_ID;
 export const AUTH_COOKIE_DOMAIN = publicRuntimeConfig.AUTH_COOKIE_DOMAIN;
 export const NODE_ENV = publicRuntimeConfig.NODE_ENV;
 export const IDP_DOMAIN = publicRuntimeConfig.IDP_DOMAIN;
 export const REDIRECT_SIGN_IN = publicRuntimeConfig.REDIRECT_SIGN_IN;
 export const REDIRECT_SIGN_OUT = publicRuntimeConfig.REDIRECT_SIGN_OUT;
-
--- next.config.js
+++ next.config.js
@@ -1,15 +1,17 @@
 const withImages = require("next-images");
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = withImages({
   publicRuntimeConfig: {
     USER_POOL_REGION: process.env.USER_POOL_REGION,
     USER_POOL_ID: process.env.USER_POOL_ID,
     USER_POOL_CLIENT_ID: process.env.USER_POOL_CLIENT_ID,
+    PUBLIC_BUCKET_NAME: process.env.PUBLIC_BUCKET_NAME,
+    IDENTITY_POOL_ID: process.env.IDENTITY_POOL_ID,
     AUTH_COOKIE_DOMAIN: process.env.AUTH_COOKIE_DOMAIN,
     NODE_ENV: process.env.NODE_ENV,
     IDP_DOMAIN: process.env.IDP_DOMAIN,
     REDIRECT_SIGN_IN: process.env.REDIRECT_SIGN_IN,
-    REDIRECT_SIGN_OUT: process.env.REDIRECT_SIGN_OUT
-  }
+    REDIRECT_SIGN_OUT: process.env.REDIRECT_SIGN_OUT,
+  },
 });
