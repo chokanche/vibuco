b1508af4afa23178c9675c171475be82a4418846
Matthijs Kralt <matthijs29@live.com>
2020-09-22 22:23:47 +0200

    Add some comments to dynamodb fetching functions

--- actions/getDataFromDDBTable.js
+++ actions/getDataFromDDBTable.js
@@ -1,6 +1,12 @@
 import { USER_POOL_REGION, IDENTITY_POOL_ID, USER_POOL_ID } from "../config";
 import AWS from "../aws.config";
 
+/**
+ * getCredentials will assign a new AWS.CognitoIdentityCredentials with the passed in token with the Identity pool configuration to the AWS.config and will return this instance too.
+ * @param token {string}
+ * @return {AWSCredentials} - Returns a AWS config credentials object
+ */
+
 const getCredentials = (token) => {
   AWS.config.credentials = new AWS.CognitoIdentityCredentials({
     IdentityPoolId: IDENTITY_POOL_ID,
@@ -13,6 +19,13 @@ const getCredentials = (token) => {
 
   return AWS.config.credentials;
 };
+
+/**
+ * getDataFromDDBTable will return a promise that will resolve with data from the DynamoDB database table
+ * @param tableName {string} - The table name in the DB that needs to be fetched
+ * @param token {string} - The authenticity token (optional) for fetching authenticated data
+ * @return {Promise<array>} - Returns a promise that will resolve in an array with records from the table in DynamoDB.
+ */
 
 const getDataFromDDBTable = (tableName, token) => {
   const credentials = getCredentials(token);
