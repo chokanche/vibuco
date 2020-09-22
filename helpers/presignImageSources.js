4f6b1037c536a1fcd63719b713340ac6824e947c
Matthijs Kralt <matthijs29@live.com>
2020-09-22 22:18:40 +0200

    Add some comments to the helper functions

--- helpers/presignImageSources.js
+++ helpers/presignImageSources.js
@@ -1,7 +1,14 @@
 import AWS from "../aws.config";
 import { USER_POOL_REGION } from "../config";
 
-const presignImageSources = async (imageData, bucketName, auth) => {
+/**
+ * presignImageSources will convert an image data array and replace the src with a presigned url
+ * @param imageData {array} - The data array coming from DynamoDB
+ * @param bucketName {string} - The bucket name that the signed urls need to be generated for
+ * @return {array} - Returns a new array with the src attribute of imageData updated with the signed url
+ */
+
+const presignImageSources = async (imageData, bucketName) => {
   const { accessKeyId, secretAccessKey, sessionToken } = AWS.config.credentials;
 
   AWS.config.update({
--- helpers/s3UrlToHttps.js
+++ helpers/s3UrlToHttps.js
@@ -1,6 +1,12 @@
 import { USER_POOL_REGION } from "../config";
 
-// This function returns a https url for accessing images from a specific bucket in our amazon s3 cloud.
+/**
+ * s3UrlToHttps takes in a s3Url (s3://bucket-name/image.jpg) and returns an actual https url
+ * @param s3Url {string}
+ * @param bucket {string} - The bucket name that the url needs to be generated for
+ * @return {string} - Returns a new https url for the image
+ */
+
 export default function s3UrlToHttps(s3Url, bucket) {
   const regexp = new RegExp(`${bucket}\/(.+)`);
 
